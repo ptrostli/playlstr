8b4320646c7f41de2075bf0f4975dfbc4a4dc4b8
Philip Trostli <philiptrostli@gmail.com>
2022-12-14 21:10:51 -0500

    serializer adjustments

--- app/serializers/track_serializer.rb
+++ app/serializers/track_serializer.rb
@@ -1,5 +1,5 @@
 class TrackSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist, :album, :length, :spotify_id
+  attributes :id, :name, :artist, :album, :length, :spotify_id, :sample, :url, :image
 
   belongs_to :playlist
 end
